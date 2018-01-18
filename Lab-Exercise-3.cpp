c55d80650cde66d08e34c2dfdd89cf5c33d85125
AM1R99 <33917997+AM1R99@users.noreply.github.com>
2018-01-18 09:06:59 +0800

    dh siap

--- Lab-Exercise-3.cpp
+++ Lab-Exercise-3.cpp
@@ -1,13 +1,18 @@
 //Display the number in reverse order
 //fix and complete the program below.
+#include <iostream>
+using namespace std;
+int main()
+{
+	int t, num, sum;
 
 
-cout<<"user input a number: ";
+cout<<"user input a number:"<<endl;
 
 for(t=num;num!=0;num=num/10)
 {
-	r=num % 10;
-	sum=sum*10+r;
+	t=num % 10;
+	sum=sum*10+t;
 
 }
 cout<<"The number in reverse order: "<<endl;
